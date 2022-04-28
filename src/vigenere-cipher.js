74f5091240160d1577e4404516e23a12e00129c4
Mazayw <mazayw@gmail.com>
2022-04-28 21:45:56 +0300

    feat: add vigenere-chiper  solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(boolean = true) {
+    this.isDirect = boolean;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  ciphering(message, key, isDirect) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    const upperMessage = message.toUpperCase();
+    const fullKey = key.repeat(Math.ceil(upperMessage.length / key.length)).toUpperCase();
+    const res = [];
+    for (let m = 0, k = 0; m < upperMessage.length; m++, k++) {
+      if (upperMessage[m].charCodeAt() == 32) k--;
+      if (upperMessage[m].charCodeAt() < 65 || upperMessage[m].charCodeAt() > 90) res.push(upperMessage[m]);
+      else {
+        let index = upperMessage.charCodeAt(m) - 65;
+        let ofset = fullKey.charCodeAt(k) - 65;
+        res.push(String.fromCharCode(65 + (isDirect ? index + ofset : index - ofset + 26) % 26));
+      }
+    }
+    if (this.isDirect) return res.join('');
+    return res.reverse().join('');
+  }
+  encrypt(message, key) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.ciphering(message, key, true);
+  }
+  decrypt(message, key) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    return this.ciphering(message, key, false);
   }
 }
 
